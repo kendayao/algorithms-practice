1609d7313be585fe28970aebb5c602fc13070ef7
Kaeneth Dayao <kendayao@gmail.com>
2021-02-23 09:13:33 -0800

    arithmetic practice js and python

---
+++ arithmetic/arithmetic.py
@@ -0,0 +1,16 @@
+def add(num1, num2):
+    return num1+num2
+
+def subtract(num1, num2):
+    return num1-num2
+
+def multiply(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    return int(num1/num2)
+
+print(add(10,24))
+print(subtract(100,91))
+print(multiply(7,70))
+print(divide(33,11))
---
+++ arithmetic/index.js
@@ -0,0 +1,20 @@
+var add = function(num1, num2){
+    return num1+num2
+}
+
+var subtract = function(num1, num2){
+    return num1-num2
+}
+
+var multiply = function(num1, num2){
+    return num1*num2
+}
+
+var divide = function(num1, num2){
+    return num1/num2
+}
+
+console.log(add(10,24))
+console.log(subtract(100,91))
+console.log(multiply(7,70))
+console.log(divide(33,11))
