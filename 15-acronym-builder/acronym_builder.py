3b31652556bceade5037fd06bd93ed722bc950f1
Kaeneth Dayao <kendayao@gmail.com>
2021-03-30 15:49:27 -0700

    acronym bulder python alogrithm

--- 15-acronym-builder/acronym_builder.py
+++ 15-acronym-builder/acronym_builder.py
@@ -1,3 +1,12 @@
 def acroynm_builder(string):
 
-    
+    stringList=string.split(' ')
+    acronymResult=[]
+
+    for word in stringList:
+        firstLetter=word[0]
+        acronymResult.append(firstLetter.upper())
+
+    return ''.join(acronymResult)
+
+print(acroynm_builder('i love programming'))
