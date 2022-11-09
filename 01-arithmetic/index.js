8984b304a467ab359bd3e1d8204fcb08d63a5fb5
Kaeneth Dayao <kendayao@gmail.com>
2022-11-08 21:30:19 -0800

    test

--- 01-arithmetic/index.js
+++ 01-arithmetic/index.js
@@ -14,7 +14,7 @@ var divide = function(num1, num2){
     return num1/num2
 }
 
-console.log(add(10,24))
+console.log(add(20,24))
 console.log(subtract(100,91))
 console.log(multiply(7,70))
 console.log(divide(33,11))
